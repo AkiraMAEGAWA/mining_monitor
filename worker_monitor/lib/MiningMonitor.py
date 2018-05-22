6b56df79ad2517dcc19302d2cf096aa7ba5fa175
maegawa-akira <maegawa-akira@Labo-MAC-0040.local>
2018-05-22 12:33:36 +0900

    config情報をMiningMonitorから隔離しました

--- worker_monitor/lib/MiningMonitor.py
+++ worker_monitor/lib/MiningMonitor.py
@@ -13,24 +13,34 @@ class MiningMonitor(object):
         import json
         import urllib.request
         from datetime import datetime
-       
+        import configparser
+
+        inifile = configparser.ConfigParser()
+        inifile.read('../config.ini', 'UTF-8')
+        # print(inifile.get("slack", "hoge"))
+        # print(inifile.get("slack", "webhook_url"))
+        # print(type(inifile.get("slack", "webhook_url")))
+
+
         # bitfly' のapiはsslの証明書を要求してくるが、その回避用
         import ssl
         ssl._create_default_https_context = ssl._create_unverified_context
 
-
-        self.slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
+        # self.webhook_url = inifile.get("slack", "webhook_url")
+        self.slack = slackweb.Slack(url=inifile.get("slack", "webhook_url"))
         # 参考ページ  https://ja.stackoverflow.com/questions/27922/python3%E3%81%A7web%E3%82%B9%E3%82%AF%E3%83%AC%E3%82%A4%E3%83%94%E3%83%B3%E3%82%B0%E3%81%97%E3%81%9F%E3%81%84%E3%81%AE%E3%81%A7%E3%81%99%E3%81%8C%E5%AD%98%E5%9C%A8%E3%81%99%E3%82%8Burl%E3%81%8C%E9%96%8B%E3%81%91%E3%81%BE%E3%81%9B%E3%82%93
         self.headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",}
 
         if _currency == "eth":
-            self.eth_url = 'https://api.ethermine.org/miner/"515CE20480D546BcbD3cd2c9f7e180e374DAE44a"/dashboard'
+            self.ethermine_wallet = inifile.get("wallet_info","ethermine_wallet")
+            self.eth_url = 'https://api.ethermine.org/miner/"{0}"/dashboard'.format(self.ethermine_wallet)
             self.request = urllib.request.Request(url=self.eth_url, headers=self.headers)
             self.res = urllib.request.urlopen(self.request)
             self.eth_data = json.loads(self.res.read().decode('utf-8'))
 
         elif _currency == "ltc":
-            self.ltc_url = 'https://www.litecoinpool.org/api?api_key=84aea38648cf63ad05aa7175a166edf4'
+            self.ltc_pool_apikey = inifile.get("wallet_info", "litecoinpool_apikey")
+            self.ltc_url = 'https://www.litecoinpool.org/api?api_key={}'.format(self.ltc_pool_apikey)
             self.request = urllib.request.Request(url=self.ltc_url, headers=self.headers)
             self.res = urllib.request.urlopen(self.request)
             self.ltc_data = json.loads(self.res.read().decode('utf-8'))
@@ -106,12 +116,17 @@ if __name__ == "__main__":
 
     active_ethworkers = eth_monitor.get_eth_currentacitiveworkers()
     active_ltcworkers = ltc_monitor.get_ltc_currentacitiveworkers()
-    print(f"ethminerのworker数は {active_ethworkers} です")
-    print(f"ltcminerのworker数は {active_ltcworkers} です")
-    eth_monitor.post2slack(_text=f"ethminerのworker数は {active_ethworkers} です")
-    ltc_monitor.post2slack(_text=f"ltcminerのworker数は {active_ltcworkers} です")
+    print("ethminerのworker数は {0} です".format(active_ethworkers))
+    print("ltcminerのworker数は {0} です".format(active_ltcworkers))
+    # eth_monitor.post2slack(_text=f"ethminerのworker数は {active_ethworkers} です")
+    # ltc_monitor.post2slack(_text=f"ltcminerのworker数は {active_ltcworkers} です")
+    eth_monitor.post2slack(_text="ethminerのworker数は {0} です".format(active_ethworkers))
+    ltc_monitor.post2slack(_text="ltcminerのworker数は {0} です".format(active_ltcworkers))
 
     red_ethworkers = eth_monitor.get_red_workers()
     print(red_ethworkers)
     for i in red_ethworkers:
-        eth_monitor.post2slack(_text=f"red worker: {i}\n")
+        # eth_monitor.post2slack(_text=f"red worker: {i}\n")
+        eth_monitor.post2slack(_text="red worker: {0}\n".format(i))
+
+
