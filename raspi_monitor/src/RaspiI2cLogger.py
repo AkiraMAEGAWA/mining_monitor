5aa6036a5122a004bfdade907d4c39851488d61d
maegawa-akira <maegawa-akira@Labo-MAC-0040.local>
2018-05-29 14:39:06 +0900

    selfの使い方ってどうだっけ

--- raspi_monitor/src/RaspiI2cLogger.py
+++ raspi_monitor/src/RaspiI2cLogger.py
@@ -11,23 +11,23 @@ class RaspiI2cLogger(object):
         self.i2c = SMBus(1)
         self.address = 0x48
 
-    def logging(self, address=self.address):
+    def logging(self):
         while True:
             try:
-                block = self.i2c.read_i2c_block_data(address, 0x00, 2)
+                block = self.i2c.read_i2c_block_data(self.address, 0x00, 2)
             except OSError as e:
                 print("Oops...  change address...")
 
-                if address == 0x48:
-                    address = 0x49
+                if self.address == 0x48:
+                    self.address = 0x49
                     print("change address to 0x49")
                     continue
-                elif address == 0x49:
-                    address = 0x4b
+                elif self.address == 0x49:
+                    self.address = 0x4b
                     print("change address to 0x4b")
                     continue
                 else:
-                    address = 0x48
+                    self.address = 0x48
                     print("change address to 0x48")
                     continue
 
