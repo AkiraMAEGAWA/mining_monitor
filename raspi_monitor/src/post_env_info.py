2e6f821cb77c948c45e4f5830da5439e70833ce6
maegawa-akira <maegawa-akira@Labo-MAC-0040.local>
2018-05-29 15:17:43 +0900

    raspiからiftttに飛ばせるように

--- raspi_monitor/src/post_env_info.py
+++ raspi_monitor/src/post_env_info.py
@@ -1,20 +1,25 @@
 import IftttSender
+import RaspiI2cLogger
 import configparser
 import datetime
 
 import sys
 args = sys.argv
 
-now = datetime.datetime.now()
-
-event_name = "add_temperature"
-if len(args) <= 1:
-    message = "hello from raspi with ifrttt v2 " + str(now.day) + "/" + str(now.hour) + "/" + str(now.minute)
-else:
-    message = str(args[1:])
-
 inifile = configparser.ConfigParser()
 inifile.read("../config/ifttt_config.ini")
 ifttt = IftttSender.IftttSender(api_key=inifile.get("webhook", "api_key"))
+
+i2c = RaspiI2cLogger.RaspiI2cLogger()
+
+now = datetime.datetime.now()
+event_name = "add_temperature"
+if len(args) <= 1:
+    message = get_temperature()
+    message.append("hello from raspi with ifrttt v2 " + str(now.day) + "/" + str(now.hour) + "/" + str(now.minute))
+else:
+    message = str(args[1:])
+
+
 ifttt.post(event_name=event_name, where_to_place="M1", message=message)
 
