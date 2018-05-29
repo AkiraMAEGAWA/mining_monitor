1127b9e47634bb47641c3f7342f791d983630402
maegawa-akira <maegawa-akira@Labo-MAC-0040.local>
2018-05-29 16:27:21 +0900

    1行目に温度を書き込めるように

--- raspi_monitor/src/post_env_info.py
+++ raspi_monitor/src/post_env_info.py
@@ -15,11 +15,10 @@ i2c = RaspiI2cLogger.RaspiI2cLogger()
 now = datetime.datetime.now()
 event_name = "add_temperature"
 if len(args) <= 1:
-    message = i2c.get_temperature()
+    message = str(i2c.get_temperature())
     message += "\n hello from raspi with ifrttt v2 " + str(now.day) + "/" + str(now.hour) + "/" + str(now.minute)
 else:
     message = str(args[1:])
 
-
-ifttt.post(event_name=event_name, where_to_place="M1", message=message)
-
+for column in [chr(i) for i in range(65,90)]:
+    ifttt.post(event_name=event_name, where_to_place=column + str(1), message=message)
