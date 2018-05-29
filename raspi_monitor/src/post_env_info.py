5dda63421d57583804b16cce172ed31438df5562
maegawa-akira <maegawa-akira@Labo-MAC-0040.local>
2018-05-29 14:09:39 +0900

    messageを変えられるようにしました

---
+++ raspi_monitor/src/post_env_info.py
@@ -0,0 +1,20 @@
+import IftttSender
+import configparser
+import datetime
+
+import sys
+args = sys.argv
+
+now = datetime.datetime.now()
+
+event_name = "add_temperature"
+if args is None:
+    message = "hello from raspi with ifrttt v2 " + str(now.day) + "/" + str(now.hour) + "/" + str(now.minute)
+else:
+    message = str(args[1:])
+
+inifile = configparser.ConfigParser()
+inifile.read("../config/ifttt_config.ini")
+ifttt = IftttSender.IftttSender(api_key=inifile.get("webhook", "api_key"))
+ifttt.post(event_name=event_name, where_to_place="M1", message=message)
+
