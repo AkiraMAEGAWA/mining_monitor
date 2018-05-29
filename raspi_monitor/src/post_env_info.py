2e7949729c6e463d6c07d57ae5888df6df989eee
maegawa-akira <maegawa-akira@Labo-MAC-0040.local>
2018-05-29 16:33:06 +0900

    loop範囲ミス

--- raspi_monitor/src/post_env_info.py
+++ raspi_monitor/src/post_env_info.py
@@ -12,13 +12,15 @@ ifttt = IftttSender.IftttSender(api_key=inifile.get("webhook", "api_key"))
 
 i2c = RaspiI2cLogger.RaspiI2cLogger()
 
-now = datetime.datetime.now()
-event_name = "add_temperature"
-if len(args) <= 1:
-    message = str(i2c.get_temperature())
-    message += "\n hello from raspi with ifrttt v2 " + str(now.day) + "/" + str(now.hour) + "/" + str(now.minute)
-else:
-    message = str(args[1:])
 
 for column in [chr(i) for i in range(65,90)]:
+
+    now = datetime.datetime.now()
+    event_name = "add_temperature"
+    if len(args) <= 1:
+        message = str(i2c.get_temperature())
+        message += "\n hello from raspi with ifrttt v2 " + str(now.day) + "/" + str(now.hour) + "/" + str(now.minute)
+    else:
+        message = str(args[1:])
+
     ifttt.post(event_name=event_name, where_to_place=column + str(1), message=message)
